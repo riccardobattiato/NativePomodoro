120269ea4b0505e4c3fd0cfaf93eab817c5ec204
riccardobattiato <rbattiato@outlook.it>
2024-08-03 12:13:35 +0200

    test(timer): useTimer hook

--- __tests__/timer.test.ts
+++ __tests__/timer.test.ts
@@ -1,5 +1,15 @@
-import { describe, expect, it } from '@jest/globals';
-import { DateTime } from 'luxon';
+import { act } from 'react';
+import { renderHook } from '@testing-library/react-native';
+import { useTimer } from '@/hooks/useTimer';
+import {
+  jest,
+  describe,
+  expect,
+  it,
+  beforeEach,
+  afterEach,
+} from '@jest/globals';
+import { DateTime, Duration } from 'luxon';
 import { getElapsedTime } from '@/lib/timer';
 
 describe('getElapsedTime', () => {
@@ -11,5 +21,47 @@ describe('getElapsedTime', () => {
 
     const diff = getElapsedTime(b, a);
     expect(diff.seconds).toBe(0);
+  });
+});
+
+describe('useTimer', () => {
+  const duration = Duration.fromObject({ minutes: 1 });
+
+  beforeEach(() => {
+    jest.useFakeTimers();
+  });
+
+  afterEach(() => {
+    jest.useRealTimers();
+    jest.clearAllTimers();
+  });
+
+  it('returns the full duration when not started', () => {
+    const { result } = renderHook(() => useTimer(duration));
+    const time = result.current.time;
+    expect(time).toEqual(duration);
+  });
+
+  it('returns the correct remaining time after start', () => {
+    const { result } = renderHook(() => useTimer(duration));
+    act(() => {
+      result.current.start();
+      jest.advanceTimersByTime(30000);
+    });
+
+    const time = result.current.time;
+    expect(time.seconds).toBe(-30);
+  });
+
+  it('returns correct remaining time after finishing', () => {
+    const { result } = renderHook(() => useTimer(duration));
+
+    act(() => {
+      result.current.start();
+      jest.advanceTimersByTime(duration.toMillis());
+    });
+
+    const time = result.current.time;
+    expect(time.seconds).toBe(-60);
   });
 });
