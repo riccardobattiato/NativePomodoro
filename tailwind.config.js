b1c386ed95938a16ebbbe199ce0cb11739c45c7f
riccardobattiato <rbattiato@outlook.it>
2024-08-10 16:40:22 +0200

    chore(tailwind): root level content purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,11 @@ const { hairlineWidth } = require('nativewind/theme');
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: 'class',
-  content: ['./app/**/*.{ts,tsx}', './components/**/*.{ts,tsx}'],
+  content: [
+    './**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+  ],
   presets: [require('nativewind/preset')],
   theme: {
     extend: {
